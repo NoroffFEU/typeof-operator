ae78a9a297b116ab55ae4b44804ade1363ee5930
Connor O'Brien <connor@hifranc.com>
2020-12-11 19:45:07 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// var pet = "dog";
+// var petAge = 11;
+// var isFriendly = false;
+
+// console.log(typeof isFriendly);
+
+var price1 = "5";
+var price2 = 4;
+
+var total = parseInt(price1) + price2;
+
+console.log("price1: ", typeof price1);
+console.log("price2: ", typeof price2);
+
+console.log("total", total);
